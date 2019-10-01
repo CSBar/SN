774235a01502988acde3086f3394eed2163f3a17
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-10-01 12:20:32 -0400

    Create config.php

---
+++ lib/config.php
@@ -0,0 +1,7 @@
+<?php
+// define("URL_BASE", "/SN/");
+define("MYSQL_DBNAME","");    // the db name
+define("MYSQL_USER", "");    // the username
+define("MYSQL_PASS", "");   // the password
+define("MYSQL_HOST","localhost");
+?>
