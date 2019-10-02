596731fa763a1479f5ed462e70080c490946f7c9
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-10-02 16:56:24 -0400

    Delete config.php

--- lib/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// define("URL_BASE", "/SN/");
-define("MYSQL_DBNAME","");    // the db name
-define("MYSQL_USER", "");    // the username
-define("MYSQL_PASS", "");   // the password
-define("MYSQL_HOST","localhost");
-?>
